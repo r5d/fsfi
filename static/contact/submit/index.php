ac11d3ec24a12dd2c79e6d4a3c15f95b3bddb274
rsiddharth <s@ricketyspace.net>
2019-08-04 11:20:37 -0400

    static/contact/submit/index.php: Update send.
    * static/contact/submit/index.php (send): Add IP address of client
      send the message.

--- static/contact/submit/index.php
+++ static/contact/submit/index.php
@@ -24,6 +24,8 @@ function send($n, $e, $m) {
     return mail(to(),
                 $p . ' sent a message to FSF India',
                 'Per says:' . PHP_EOL . PHP_EOL . $m
+              . PHP_EOL . PHP_EOL . '---' . PHP_EOL
+              . 'Message originating from ' . $_SERVER['REMOTE_ADDR']
     );
 }
 
