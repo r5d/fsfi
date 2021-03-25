28d20b56e6afb8610245aec8639bfe752b7f466b
rsiddharth <s@ricketyspace.net>
2021-03-24 20:34:15 -0400

    contact: also send to ravi@fsf.org.in

--- static/contact/submit/index.php
+++ static/contact/submit/index.php
@@ -9,7 +9,7 @@
  */
 
 function to() {
-    return 'praveen@onenetbeyond.org, ravi@anche.no';
+    return 'praveen@onenetbeyond.org, ravi@anche.no, ravi@fsf.org.in';
 }
 
 function email_valid($e) {
